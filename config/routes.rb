41135372e8126a89eb652415d86ad6a5aa44fc35
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-10 21:23:24 +0500

    edited show page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'users/show'
   get '/users', to: 'users#index'
   root 'users#show'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
