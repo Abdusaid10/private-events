ea10dd035e0b1155c98d717147d6a290cd88e451
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-10 20:27:37 +0500

    set up routes and fix type error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,9 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       render 'new'
+    end
   end
-  
+
   def destroy
     log_out
     redirect_to root_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get 'users/show'
-  get 'users/create'
+  get '/users', to: 'users#index'
+  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
