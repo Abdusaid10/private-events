bb979f596e256ec7e92d93ee28ecc35b3f5c16dc
imhta <imhtapm@gmail.com>
2019-09-11 17:17:14 +0530

    create event with association build method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,8 +14,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.user_id = session[:user_id]
+    @event = current_user.events.build(event_params)
     if @event.save
       redirect_to @event
     else
