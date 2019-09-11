f67419b4ee5886a2ef29185ec93393486fe1978b
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-11 19:36:51 +0500

    fix offesnes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-
+  has_many :events
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-            format: { with: VALID_EMAIL_REGEX},
-            uniqueness: { case_sensitive: false }
-  has_many :events
 end
