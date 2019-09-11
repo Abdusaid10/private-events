4c8b7a0abfb3cb6a220d8f1756197a59e0e7b6d2
imhta <imhtapm@gmail.com>
2019-09-11 17:07:15 +0530

    basic new event template

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'users/show'
   get '/users', to: 'users#index'
   root 'users#new'
+  resources :events
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
