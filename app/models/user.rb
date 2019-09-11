6ff3d3f70fb9f6cad0b52fe147dc5a5883c206cd
imhta <imhtapm@gmail.com>
2019-09-11 21:06:21 +0530

    rubocop autofix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   has_many :events, class_name: 'Event', foreign_key: 'user_id'
   has_many :attendances
-  has_many :events_as_attendee, through: :attendances,  source: "event"
+  has_many :events_as_attendee, through: :attendances, source: 'event'
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
