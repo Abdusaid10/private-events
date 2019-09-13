9442dd67969e81cafcc7aa0814735236a759f763
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-13 19:04:01 +0500

    added validations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,8 @@ class Event < ApplicationRecord
   has_many :attendees, through: :attendances, source: 'user'
   scope :upcomming, -> { where('date > ?', DateTime.now) }
   scope :past, -> { where('date < ?', DateTime.now) }
+  validates :title, presence: true, length: { maximum: 100 }
+  VALID_URL = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  validates :url, presence: true, format: { with: VALID_URL}
+  validates :date, presence: true
 end
