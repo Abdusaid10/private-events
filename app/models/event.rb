163dd22ab395b993c0a31049063506d3f9e45f95
imhta <imhtapm@gmail.com>
2019-09-13 20:24:53 +0530

    rubocop autofix

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
   scope :upcomming, -> { where('date > ?', DateTime.now) }
   scope :past, -> { where('date < ?', DateTime.now) }
   validates :title, presence: true, length: { maximum: 100 }
-  VALID_URL = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
-  validates :url, presence: true, format: { with: VALID_URL}
+  VALID_URL = %r{\A(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix.freeze
+  validates :url, presence: true, format: { with: VALID_URL }
   validates :date, presence: true
 end
