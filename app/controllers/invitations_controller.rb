7c4bd37a3011b9e6692935e537c0db482a25e690
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-15 19:15:38 +0500

    modify invitations controller and model

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,8 +1,44 @@
 class InvitationsController < ApplicationController
+  
+  def index
+  end
   def new
-    @invitation=Invitaion.new
+    @invitation = Invitation.new
   end
 
+  def create
+    event=Event.find(params[:invitation][:event])
+    user=User.find_by(email: params[:invitation][:invited_user])
+
+    @invitation = Invitation.new
+
+    if user && user.events_as_attendee.find_by(id: event.id) == nil && user.invitations_received.find_by(event: event) == nil
+      @invitation=current_user.invitations.new(event: event, invited_user: user)
+    elsif user
+      flash.now[:notice]="User already invited"
+    else
+      flash.now[:notice]="User doesn't exist"
+    end
+
+    if @invitation.save
+      flash[:notice]="Invitation sent"
+      redirect_to root_url
+    else 
+      render :new
+    end
+  end
+  
   def show
   end
+
+  def destroy 
+    @invitation =Invitation.find(params[:id]).destroy
+    flash[:notice] ="Invitation declined"
+    redirect_to root_url
+  end
+
+  # private
+  # def invite_params
+  #   params.require(:invitation).permit(:email)
+  # end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,15 @@
 class Invitation < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'event_creator'
-  belongs_to :event, class_name: 'Event' foreign_key: 'event_id'
+  belongs_to :event, class_name: 'Event', foreign_key: 'event_id'
   belongs_to :invited_user, class_name: 'User', foreign_key: 'invited_user'
+ 
+  before_save :check_user_existence
+
+  def check_user_existence
+    user= User.find_by_email(email)
+    if user
+      self.invited_user = user.id
+    end
+  end
+
 end
