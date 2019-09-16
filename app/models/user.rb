ee1ad139bed46f7cc37f0f914eb0f7d5092be28b
imhta <imhtapm@gmail.com>
2019-09-16 20:13:13 +0530

    spelling correction in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events_as_attendee, through: :attendances, source: 'event'
   has_many :invitations, class_name: 'Invitation', foreign_key: 'invitation_creator'
-  has_many :invitatinos_received, class_name: 'Invitation', foreign_key: 'invited_user'
+  has_many :invitations_received, class_name: 'Invitation', foreign_key: 'invited_user'
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
