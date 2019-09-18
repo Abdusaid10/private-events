26cfe29e351e5c36b9d79a4eee7ca142174cb5d6
imhta <imhtapm@gmail.com>
2019-09-18 15:08:45 +0530

    added create action for invitation

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :events
   resources :users
-  resources :invitations, only: [:show]
+  resources :invitations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
