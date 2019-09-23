d52525649be84bf56127d7224c617361eaa78d2b
imhta <imhtapm@gmail.com>
2019-09-23 20:47:29 +0530

    event create test added

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,25 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  before :each do
+    @user = User.find_by(email: 'test@test.com')
+  end
   it 'creates new valid event' do
-    event = Event.new(title: 'New event', url: 'https://www.gmail.com', date: 3.days.after, user_id: 1)
+    event = @user.events.create(title: 'New event', description: '', url: 'https://www.gmail.com', date: 3.days.after)
     expect(event).to be_valid
   end
 
   it 'is invalid without event title' do
-    event = Event.new(title: nil, url: 'https://www.gmail.com', date: 3.days.after, user_id: 1)
+    event = @user.events.new(title: nil, url: 'https://www.gmail.com', date: 3.days.after)
     event.valid?
     expect(event.errors[:title]).to include("can't be blank")
   end
 
   it 'is invalid without event email' do
-    event = Event.new(title: 'New event', url: nil, date: 3.days.after, user_id: 1)
+    event = @user.events.new(title: 'New event', url: nil, date: 3.days.after)
     event.valid?
     expect(event.errors[:url]).to include("can't be blank")
   end
 
   it 'is invalid with event title longer than specified' do
-    event = Event.new(title: 'ababa' * 100, url: 'https://www.gmail.com', date: 3.days.after, user_id: 1)
+    event = @user.events.new(title: 'ababa' * 100, url: 'https://www.gmail.com', date: 3.days.after)
     event.valid?
     expect(event.errors[:title]).to include('is too long (maximum is 100 characters)')
   end
